e215667420bf1271583a5a0fc7c966f4b9fdf4fd
Natthaporn <natthaporn.som@g.swu.ac.th>
2017-11-19 15:33:10 +0700

    first commit

---
+++ src/routes.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import { Switch, Route } from 'react-router-dom'
+
+import Login from './pages/Login'
+import Main from './pages/Main'
+const Routes =() => {
+    return (
+    <Switch>
+    <Route exact path='/login' component={Login} />
+    <Route exact path='/' component={Main} />
+  </Switch>
+    )
+
+}
+export default Routes
